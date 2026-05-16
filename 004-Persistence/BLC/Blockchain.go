ca557df586a8a6c3660858f4d5940361871823aa
kuni <liheath230@gmail.com>
2026-05-16 16:10:27 -0400

    实现了迭代器，并且优化代码分离解耦

--- 004-Persistence/BLC/Blockchain.go
+++ 004-Persistence/BLC/Blockchain.go
@@ -17,47 +17,36 @@ type Blockchain struct {
 	DB  *bolt.DB
 }
 
+// 4.迭代器
+func (blockchain *Blockchain) Iterator() *BlockchainIterator {
+
+	return &BlockchainIterator{blockchain.Tip, blockchain.DB}
+}
+
 // 3.遍历输出所有区块的信息
 func (blc *Blockchain) Printchain() {
 
-	var block *Block
-	var currentHash []byte = blc.Tip
+	blockchainIterator := blc.Iterator()
 
 	for {
-		err := blc.DB.View(func(tx *bolt.Tx) error {
-			//1. 读写表
-			b := tx.Bucket([]byte(blockTableName))
-			if b != nil {
-				//获取当前区块的字节数组
-				blockBytes := b.Get(currentHash)
-				//反序列化
-				block = DeserializeBlock(blockBytes)
+		block := blockchainIterator.Next()
 
-				fmt.Printf("Height: %d\n", block.Height)
-				fmt.Printf("PrevBlockHash: %x\n", block.PrevBlockHash)
-				fmt.Printf("Data: %s\n", block.Data)
-				fmt.Printf("Timestamp: %s\n", time.Unix(block.Timestamp, 0).Format("2006-01-02 03:04:05 PM"))
-				fmt.Printf("Hash: %x\n", block.Hash)
-				fmt.Printf("Nonce: %d\n", block.Nonce)
-			}
-
-			return nil
-		})
+		fmt.Printf("Height: %d\n", block.Height)
+		fmt.Printf("PrevBlockHash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Timestamp: %s\n", time.Unix(block.Timestamp, 0).Format("2006-01-02 03:04:05 PM"))
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("Nonce: %d\n", block.Nonce)
 
 		fmt.Println()
 
-		if err != nil {
-			log.Panic(err)
-		}
-
 		var hashInt big.Int
 		hashInt.SetBytes(block.PrevBlockHash)
-
 		if big.NewInt(0).Cmp(&hashInt) == 0 {
 			break
 		}
-		currentHash = block.PrevBlockHash
 	}
+
 }
 
 // 2.增加区块到区块链里面
